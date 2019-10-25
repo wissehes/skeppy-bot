ee125c49d52d573f546a422b6e08ed0190eb3dc1
thechicken14 <wissehes@gmail.com>
2019-10-25 22:56:28 +0200

    uh ooh

--- commands/braincells.js
+++ commands/braincells.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-  var braincells = Math.floor((Math.random() * 1000) + -1000);
+  var braincells = Math.floor((Math.random() * 1000) + -100);
   message.reply('you have exactly '+braincells+' braincells left');
 }
