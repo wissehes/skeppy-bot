1142f2bb102dc856813ac21ec3bc1ad24fbfdb50
thechicken14 <wissehes@gmail.com>
2019-10-26 17:52:02 +0200

    added announcement

---
+++ commands/sendannouncement.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+  if(message.author.id !== client.config.ownerID) return;
+      var guildList = client.guilds.array();
+      const sayMessage = args.join(" ");
+      try {
+          guildList.forEach(guild => guild.systemChannel.send(sayMessage));
+      } catch (err) {
+          console.log("Could not send message to " + guild.name);
+      }
+}
