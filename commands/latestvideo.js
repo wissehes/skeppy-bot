72748c98949da8d088f80a8cae4457cdae1f7e85
thechicken14 <wissehes@gmail.com>
2019-10-27 15:52:09 +0100

    the latestvideo now works for every channel yay

--- commands/latestvideo.js
+++ commands/latestvideo.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
   var request = require('request');
-  var channel = args[0]
+  var channel = args.join(" ");
   if(args[0] === undefined) {
     request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId=UCzMjRlKVO9XIqH_crIFpi6w&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
       if (!error && response.statusCode == 200) {
@@ -10,29 +10,19 @@ exports.run = (client, message, args) => {
       }
     });
   }
-  if(channel === 'skeppy' || channel === 'Skeppy') {
-    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId=UCzMjRlKVO9XIqH_crIFpi6w&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
+  if(channel) {
+    var channelid;
+    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&q='+channel+'&part=snippet,id&order=relevance&maxResults=1&type=channel', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body)
-        message.channel.send("This is Skeppy's latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
-        console.log(info)
-      }
-    });
-  }
-  if(channel === 'BadBoyHalo' || channel === 'badboyhalo' || channel === 'bbh'){
-    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId=UC0lkCn7D--uMrtwAXRj_cdw&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body)
-        message.channel.send("This is BadBoyHalo's latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
-        console.log(info)
-      }
-    });
-  }
-  if(channel === 'a6d' || channel === 'A6d' || channel === 'A6D'){
-    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId=UCbyS9AQt6KE0XUuGqXya90A&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body)
-        message.channel.send("This is a6d's latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
+        var channelid = info.items[0].id.channelId;
+        request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId='+channelid+'&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body)
+            message.channel.send("This is "+channel+"'s latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
+            console.log(info.items[0].id.videoId)
+          }
+        });
         console.log(info)
       }
     });
