ab119722318c0fef1b8a4216a260c62b3a4ceb72
thechicken14 <wissehes@gmail.com>
2019-10-28 16:24:38 +0100

    recoded the command to use Axios instead of request

--- commands/latestvideo.js
+++ commands/latestvideo.js
@@ -1,31 +1,32 @@
 exports.run = (client, message, args) => {
-  var request = require('request');
+  var axios = require('axios');
   var channel = args.join(" ");
-  if(args[0] === undefined) {
-    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId=UCzMjRlKVO9XIqH_crIFpi6w&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body)
-        message.channel.send("This is Skeppy's latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
-        console.log(info)
-      }
-    });
-  }
-  if(channel) {
-    var channelid;
-    request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&q='+channel+'&part=snippet,id&order=relevance&maxResults=1&type=channel', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body)
-        var channelid = info.items[0].id.channelId;
-        request('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId='+channelid+'&part=snippet,id&order=date&maxResults=1', function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            var info = JSON.parse(body)
-            message.channel.send("This is "+channel+"'s latest video is: https://youtube.com/watch?v="+ info.items[0].id.videoId);
-            console.log(info.items[0].id.videoId)
-          }
-        });
-        console.log(info)
-      }
-    });
+  if(args[0]){
+    getVideo(channel)
+  }else{
+    message.channel.send(`🚫 You didn't specify a channel name!`)
   }
 
+  function getVideo(channelname) {
+    axios.get('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&q='+channelname+'&part=snippet,id&order=relevance&maxResults=1&type=channel')
+    .then(res => {
+        const body = res.data;
+        var channelid = body.items[0].id.channelId;
+        return channelid;
+    }).then(channelid => {
+      axios.get('https://www.googleapis.com/youtube/v3/search?key='+client.config.YTapikey+'&channelId='+channelid+'&part=snippet,id&order=date&maxResults=1')
+      .then(res => {
+        const body = res.data;
+        var lastVideoId = body.items[0].id.videoId;
+        message.channel.send("This is "+channel+"'s latest video is: https://youtube.com/watch?v="+ lastVideoId)
+        return lastVideoId;
+      }).catch(err => {
+          // Handle the error somehow.
+          console.log(err)
+      });
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  }
 }
