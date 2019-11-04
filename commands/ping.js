5efb507009dae5adb95d77da5374fc46aa70a6be
thechicken14 <wissehes@gmail.com>
2019-11-04 16:24:30 +0100

    pingi

--- commands/ping.js
+++ commands/ping.js
@@ -1,3 +1,5 @@
 exports.run = (client, message, args) => {
-  message.channel.send(`spoof!`);
-}
+    message.channel.send("Ping?").then(m =>{
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    })
+  }
