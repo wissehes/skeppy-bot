ea627bd5f16096eed2c26f37cc0556f047c32f19
thechicken14 <wissehes@gmail.com>
2019-11-04 17:00:53 +0100

    yes

--- commands/servers.js
+++ commands/servers.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-message.channel.send(`I'm in ${client.guilds.size} with ${client.guilds.size} members.`)
+message.channel.send(`I'm in ${client.guilds.size} servers with ${client.users.size} members and ${client.channels.size} channels.`)
 }
