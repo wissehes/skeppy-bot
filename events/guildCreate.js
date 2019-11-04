aa4107e505418a41a79d6edfe969706e348ac8e1
thechicken14 <wissehes@gmail.com>
2019-11-04 14:18:48 +0100

    uwu

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,4 +3,5 @@ module.exports = (client, guild) => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`for "${client.config.prefix}help" | Serving ${client.guilds.size} servers`, {type: "WATCHING"});
+  client.channels.get(client.config.NewServerChannel).send(`🎉 I was added to a new server: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members!`)
 }
