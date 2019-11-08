5da40fb8f335bf55d826f6f0d8d4358ed1ab56f0
thechicken14 <wissehes@gmail.com>
2019-11-08 20:14:52 +0100

    chrismaz

---
+++ commands/christmas.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+    if (args[0] === `christmas`){
+        var serverInvite = "https://discord.gg/zkyNyfb";
+        message.channel.send(`The invite link for the Trio Christmas Server is ${inviteLink}`)
+    }
+}
