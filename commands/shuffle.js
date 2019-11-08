c3666b9e3615cf0e493473e0ec01691cf2b06ebb
thechicken14 <wissehes@gmail.com>
2019-11-08 21:39:29 +0100

    shuffle uwu

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
     if(!queue || queue.length == 0)
-    return message.channel.send(`No music is playing!`);
+        return message.channel.send(`No music is playing!`);
     
     if(!client.musicSettings[message.guild.id])
         client.musicSettings[message.guild.id] = {loop:0,shuffle:false};
