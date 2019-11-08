ab12435edf8156e674eb2b970708243bb33c19e9
thechicken14 <wissehes@gmail.com>
2019-11-08 14:52:22 +0100

    some modifications uwu

--- commands/q.js
+++ commands/q.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
-//shortcut for queue
+
 exports.run = (client, message, args) => {
   var queue = client.getQueue(message.guild.id);
   if(!queue || queue.length == 0)
     return message.channel.send(`No music is playing!`);
-
+    
   var list = queue.map((a, i) => `**${i + 1}**. ${a.info.author ? `[**${a.info.title}**](${a.info.uri}) by **${a.info.author}**` : `Unknown`} (${client.getYTLength(a.info.length)})`);
-
+  
   if(list.length > 15)
     list.splice(14, list.length);
 
@@ -14,6 +14,9 @@ exports.run = (client, message, args) => {
   embed.setColor('RED').setTitle(`Queue for **${message.guild.name}**`);
 
   embed.setDescription(list.join('\n'));
-
+  if(client.musicSettings[message.guild.id])
+    embed.addField('Current Settings', `**Loop**: ${client.musicSettings[message.guild.id].loop}\n**Shuffle**: ${client.musicSettings[message.guild.id].shuffle}`);
+  else
+    embed.addField('Current Settings', `**Loop**: 0\n**Shuffle**: false`);
   message.channel.send(embed);
 }
--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,4 +1,7 @@
 exports.run = async (client, message, args) => {
+    if(!queue || queue.length == 0)
+    return message.channel.send(`No music is playing!`);
+    
     if(!client.musicSettings[message.guild.id])
         client.musicSettings[message.guild.id] = {loop:0,shuffle:false};
 
