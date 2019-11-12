0cb4fbca14913bb288f862e3ea31a0efdcd24616
thechicken14 <wissehes@gmail.com>
2019-11-12 08:56:58 +0100

    seek command!

---
+++ commands/seek.js
@@ -0,0 +1,18 @@
+exports.run = (client, message, args) => {
+    var queue = client.getQueue(message.guild.id);
+    if(!queue || queue.length == 0)
+      return message.channel.send(`No music is playing!`);
+    
+    if(!message.member.voiceChannelID)
+      return message.channel.send(`You're not in a voice channel!`);
+  
+    if(client.player.get(message.guild.id) && message.member.voiceChannelID !== client.player.get(message.guild.id).channel)
+      return message.channel.send(`You're not in the playing voice channel!`);
+    
+    var pos = args[0] * 1000
+    if(!pos || pos.length < 1)
+        return message.channel.send(`You must define a position in seconds`)
+    
+    message.channel.send(`Position set to ${pos / 1000} seconds`)
+    client.player.get(message.guild.id).seek(pos)   
+}
