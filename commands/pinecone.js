0e01d4f6edae3f3c3361e71050b051dc200ccedf
TheChicken14 <wissehes@gmail.com>
2019-11-19 13:11:37 +0100

    yayyyy pinecones!

--- commands/pinecone.js
+++ commands/pinecone.js
@@ -1,3 +1,28 @@
-exports.run = (client, message, args) => {
-  message.channel.send("Here's your pinecone:",{ file: "https://i.imgur.com/RUKePmB.png"});
+const Canvas = require('canvas');
+const Discord = require('discord.js');
+//ty discordjs.guide
+exports.run = async (client, message, args) => {
+    if(args[0]){
+        const user = message.mentions.users.first()
+        if(user){
+            return createImage(user);
+        }
+        if (!user) {
+			      return message.reply('Are you stupid? Just mention someone, smh');
+		    }
+    }
+    createImage(message.author)
+    async function createImage(userMention) {
+        const canvas = Canvas.createCanvas(474, 474);
+        const ctx = canvas.getContext('2d');
+        const background = await Canvas.loadImage('./img/pinecone.jpg');
+        // This uses the canvas dimensions to stretch the image onto the entire canvas
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        const avatar = await Canvas.loadImage(userMention.avatarURL);
+        ctx.drawImage(avatar, 167.1, 142.6, 130, 130);
+    
+        // Use helpful Attachment class structure to process the file for you
+        const attachment = new Discord.Attachment(canvas.toBuffer(), 'pinecone.png');
+        message.channel.send(attachment)
+    }
 }
