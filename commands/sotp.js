1515d0f6f7cbfa3182d142058afde7ba62a1d0b5
TheChicken14 <wissehes@gmail.com>
2019-11-19 17:23:40 +0100

    e

--- commands/sotp.js
+++ commands/sotp.js
@@ -18,6 +18,6 @@ exports.run = (client, message, args) => {
     //ignore dm's
   if (message.channel.type === 'dm')
       return message.channel.send(`You need to be in a server to use this command.`);
-  const cmd = client.commands.get('queue');
+  const cmd = client.commands.get('stop');
   cmd.run(client, message, args);
 }
