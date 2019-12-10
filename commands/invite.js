03f91ab0880bf7096204e258fe53f3efb81a0d70
TheChicken14 <wissehes@gmail.com>
2019-12-10 18:12:22 +0100

    invite cmd

---
+++ commands/invite.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send(`Invite me with this link: https://discordapp.com/oauth2/authorize?client_id=579327336395309062&scope=bot&permissions=2084044031\nVote for me at: https://top.gg/bot/579327336395309062`)
+}
