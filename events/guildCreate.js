17494e9662036e8bdeccb3137f6bf778c732d656
TheChicken14 <wissehes@gmail.com>
2019-12-22 17:02:44 +0100

    This bitch was abusing my thingy :((

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,8 +4,8 @@ module.exports = (client, guild) => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`for "${client.config.prefix[0]}help" in ${client.guilds.size} servers | skeppybot.xyz`, {type: "WATCHING"});
   var text = `🎉 I was added to a new server: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members!`
-  if(text.includes(`@everyone`))
-    text = text.replace(`@everyone`, '`@everyone`')
+  if(text.includes(`@everyone`) || text.includes(`https://`) || text.includes(`http://`) || text.includes(`<@`))
+    return;
   
   client.channels.get(client.config.NewServerChannel).send(text)
 }
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -3,5 +3,9 @@ module.exports = (client, guild) => {
     // This event triggers when the bot joins a guild.
     console.log(`Removed from: ${guild.name} (id: ${guild.id}). This guild had ${guild.memberCount} members!`);
     client.user.setActivity(`for "${client.config.prefix[0]}help" in ${client.guilds.size} servers | skeppybot.xyz`, {type: "WATCHING"});
-    client.channels.get(client.config.NewServerChannel).send(`😕 I was removed from: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members`)
+    var text = `😕 I was removed from: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members`
+    if(text.includes(`@everyone`) || text.includes(`https://`) || text.includes(`http://`) || text.includes(`<@`))
+      return;
+
+    client.channels.get(client.config.NewServerChannel).send()
   }
