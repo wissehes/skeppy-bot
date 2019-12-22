15fb44ca604eca25009580230f30d5caea878f09
TheChicken14 <wissehes@gmail.com>
2019-12-22 22:52:20 +0100

    my god

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,15 @@
 exports.run = (client, message, args) => {
   var memberTag = message.author.username
-  if(args.includes(`@everyone`, `@here`) || message.mentions.users.first()){
+  const sayMessage = args.join(" ");
+
+  /*if(args.includes(`@everyone`) || args.includes(`@here`) || message.mentions.users.first()){
     return message.reply(`You can't mention anyone!`)
-  }
+  }*/
   // makes the bot say something and delete the message. As an example, it's open to anyone to use.
   // To get the "message" itself we join the `args` back into a string with spaces:
-  const sayMessage = args.join(" ");
   // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
   message.delete();
   // And we get the bot to say the thing:
-  message.channel.send("**" + memberTag + ": **" + sayMessage);
+  //message.channel.send("**" + memberTag + ": **");
+  message.channel.send(sayMessage, {code: true})
 }
