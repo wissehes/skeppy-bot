1e100c95012a3e780ec5bad02901a7c162c3d367
TheChicken14 <wissehes@gmail.com>
2019-12-22 16:58:24 +0100

    this bitch

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,5 +3,9 @@ module.exports = (client, guild) => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`for "${client.config.prefix[0]}help" in ${client.guilds.size} servers | skeppybot.xyz`, {type: "WATCHING"});
-  client.channels.get(client.config.NewServerChannel).send(`🎉 I was added to a new server: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members!`)
+  var text = `🎉 I was added to a new server: ${guild.name} (id: ${guild.id}). With ${guild.memberCount} members!`
+  if(text.includes(`@everyone`))
+    text = text.replace(`@everyone`, '`@everyone`')
+  
+  client.channels.get(client.config.NewServerChannel).send(text)
 }
