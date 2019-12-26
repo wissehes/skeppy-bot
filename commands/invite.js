556bd4f3481f2a3745376345ed6b73cfbf7f66f8
TheChicken14 <wissehes@gmail.com>
2019-12-27 00:32:46 +0100

    :)

--- commands/invite.js
+++ commands/invite.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
-    .setTitle(`Invite links`)
+    .setTitle(`Here are some links`)
     .setDescription(`
 [Invite](https://discordapp.com/oauth2/authorize?client_id=579327336395309062&scope=bot&permissions=2084044031)
 [Vote](https://top.gg/bot/579327336395309062)
