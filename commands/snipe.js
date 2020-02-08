f8cf5add307367f0def9c8ed0cdcb485e9992a70
Wisse <wissehes@gmail.com>
2020-02-08 22:29:31 +0100

    Update snipe.js

--- commands/snipe.js
+++ commands/snipe.js
@@ -3,7 +3,7 @@ const { RichEmbed } = require("discord.js")
 exports.run = (client, message, args) => {
     const DB = client.snipes
     if(client.snipes.has(message.channel.id)){
-        const DBResult = client.snipes.get(message.guild.id, message.channel.id)
+        const DBResult = client.snipes.get(message.channel.id)
         const author = client.users.get(DBResult.author)
         const embed = new RichEmbed()
             .setAuthor(author.tag, author.avatarURL)
