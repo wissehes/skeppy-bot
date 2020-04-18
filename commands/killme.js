a706f387513ad22f6095aa907f004bf1e29cee15
Wisse <wissehes@gmail.com>
2020-04-18 14:58:25 +0200

    oops

--- commands/killme.js
+++ commands/killme.js
@@ -1,15 +1,15 @@
 const Canvas = require('canvas');
 const Discord = require('discord.js');
 //ty discordjs.guide
-exports.run = async (client, message, args) => {
-    if(args[0]){
+exports.run = async(client, message, args) => {
+    if (args[0]) {
         const user = message.mentions.users.first()
-        if(user){
+        if (user) {
             return createImage(user);
         }
         if (!user) {
-			return message.reply('Are you stupid? Just mention someone, smh');
-		}
+            return message.reply('Are you stupid? Just mention someone, smh');
+        }
     }
     createImage(message.author)
     async function createImage(userMention) {
@@ -18,11 +18,11 @@ exports.run = async (client, message, args) => {
         const background = await Canvas.loadImage('./img/furry.jpg');
         // This uses the canvas dimensions to stretch the image onto the entire canvas
         ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-        const avatar = await Canvas.loadImage(userMention.avatarURL);
+        const avatar = await Canvas.loadImage(userMention.displayAvatarURL({ format: 'jpg' }));
         ctx.drawImage(avatar, 256.5, 145.1, 145, 145);
-    
+
         // Use helpful Attachment class structure to process the file for you
-        const attachment = new Discord.Attachment(canvas.toBuffer(), 'furry.png');
+        const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'furry.png');
         message.channel.send(attachment)
     }
 }
@@ -32,4 +32,4 @@ exports.info = {
     description: `Puts your avatar on a furry!`,
     usage: `killme [mention]`,
     category: `Fun`
-  }
+}
