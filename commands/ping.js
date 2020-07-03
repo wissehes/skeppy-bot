3f78a4fa40fab8145a7531f01d5c60d741bee023
Wisse <wissehes@gmail.com>
2020-07-03 10:44:53 +0200

    Fixed the NaNms thing

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     message.channel.send("Ping?").then(m =>{
-      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
     })
 }
 exports.info = {
