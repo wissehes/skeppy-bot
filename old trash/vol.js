291c9c86abc29e0b9969f5747b2702b601b8aade
Wisse <wissehes@gmail.com>
2020-07-23 16:27:09 +0200

    removed old trash <3

--- old trash/vol.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-//Shortcut for volume
-exports.run = (client, message, args) => {
-  //ignore dm's
-  if (message.channel.type === 'dm')
-      return message.channel.send(`You need to be in a server to use this command.`);
-  const cmd = client.commands.get('volume');
-  cmd.run(client, message, args);
-}
