63da16c21935f4ada2780c512385cfa08f5c6500
Wisse <wissehes@gmail.com>
2020-08-16 12:44:46 +0200

    fixed it

--- commands/pingspoof.js
+++ commands/pingspoof.js
@@ -1,14 +1,14 @@
 exports.run = (client, message, args) => {
   function getPingspoof() {
     var pings = ['Yes that is ONLY now allowed, do not tell anyone about this, yes?', '14', 'sotp with pingspoofing me! :disappointed_relieved: Why you bully me?!', 'no pls', 'sotp plsz'];
-    return pings[Math.floor(Math.random()*pings.length)];
+    return pings[Math.floor(Math.random() * pings.length)];
   }
-    var pingspoof = getPingspoof()
-    message.channel.send(pingspoof)
+  var pingspoof = getPingspoof()
+  message.channel.send(pingspoof)
     .then(msg => {
-      msg.delete(5000)
+      msg.delete({ timeout: 5000 })
     })
-  .catch(/*Your Error handling if the Message isn't returned, sent, etc.*/);
+    .catch(console.error);
 }
 exports.info = {
   name: `pingspoof`,
